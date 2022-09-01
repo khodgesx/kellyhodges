3cbe2367d90749cc9056d262867129dfa642280f
khodges <zkellyhodges@gmail.com>
2022-09-01 13:46:33 -0700

    updated api to be joke instead of quote, update styling for jokes and joke button, and changed from jquery to vanilla js for joke functionality

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
 
-let i = 0;
-let txt = 'Welcome to my website';
-let speed = 50;
+const button = document.getElementById('get-joke')
+const welcomeBox = document.getElementById('welcome-section')
+const jokeP = document.getElementById('joke')
 
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("welcome").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
-// const typeWriter = () => {
-//     if (i < txt.length) {
-//       $('#welcome').innerHTML += txt.charAt(i);
-//       i++;
-//       setTimeout(typeWriter, speed);
-//     }
-//   }
 
-//quote button for quote api
-async function quotes(){
+//Joke function for Joke api
+const jokes = async()=>{
 
-  const urlString = 'https://api.github.com/zen';
+  //old linke for code zen: https://api.github.com/zen
+  const urlString = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single';
+
   const response = await fetch(urlString);
-  const results = await response.text();
+  const results = await response.json();
+  
+  results.joke.length > 100 ? $('#welcome-section').addClass('big-joke-area') && $('#joke').addClass('big-joke-font')
+  : $('#welcome-section').removeClass('big-joke-area') && $('#joke').removeClass('big-joke-font')
+  
+  results.joke.length > 50 ? 
+  jokeP.classList.add('big-joke-font') 
+  && 
+  welcomeBox.classList.add('big-joke-area')
+  :
+  jokeP.classList.remove('big-joke-font') 
+  && 
+  welcomeBox.classList.remove('big-joke-area')
 
-  $('#quote').html(results)
+  //set joke text for joke <p> element
+  jokeP.innerHTML = results.joke;
+  
+
   
 }
-$('#get-quote').on('click', quotes);
+
+//Event Listener
+button.addEventListener('click', jokes)
